154f24e7a42ef56a73425869cc967c8aabf5251c
Jonathan Selander <jonathan@madepeople.se>
2016-01-05 11:06:08 +0100

    Do not treat errors as array if they are null

--- src/app/code/community/Vaimo/Klarna/controllers/Checkout/KlarnaController.php
+++ src/app/code/community/Vaimo/Klarna/controllers/Checkout/KlarnaController.php
@@ -694,8 +694,10 @@ class Vaimo_Klarna_Checkout_KlarnaController extends Mage_Core_Controller_Front_
                 $errors = $cart->getQuote()->getErrors();
                 $messages = array();
 
-                foreach ($errors as $error) {
-                    $messages[] = $error->getCode();
+                if (!empty($errors)) {
+                    foreach ($errors as $error) {
+                        $messages[] = $error->getCode();
+                    }
                 }
 
                 if (count($messages) > 0) {
